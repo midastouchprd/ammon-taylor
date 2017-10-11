0de740515dbfd7ca076670d0af2e5ebe35e8a35b
Tosin Awofeso <tosin@midastouchmedia.com>
2017-10-11 15:11:55 -0500

    copy lain

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 $(function () {
-  $('#showMailChimp').click(() => {
-    $('.form-overlay').fadeIn(400);
-
-    $('#mc-embedded-subscribe').click(() => {
-      $('.form-overlay').fadeOut(400);
-    })
-  })
+  console.log('booya');
 
   $(window).scroll((event) => {
     $('.background-image img').css('transform', `translateY(${-(($(document).scrollTop()) / 10)}px)`)
